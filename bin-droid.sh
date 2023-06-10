28079de4c2cb5d60ec067c685510d83ade38cb3e
MRX90902WX <118016739+MRX90902WX@users.noreply.github.com>
2023-06-09 19:16:42 -0500

    Add files via upload

---
+++ bin-droid.sh
@@ -0,0 +1,29 @@
+#! bin/bash
+
+clear
+setterm -foreground green
+cat android.txt
+echo ""
+while :
+do
+echo -e "\e[1;32m[1]-[ GENERAR BIN CON FECHA ]\e[0m"
+echo -e "\e[1;32m[2]-[ GENERAR BIN RANDOM ]\e[0m"
+echo ""
+echo -e -n "\e[1;31m》》》\e[0m "
+read opcion
+case $opcion in
+1)
+#! bin/bash
+echo ""
+python bfc.py
+exit
+;;
+2)
+#/ bin/bash
+echo ""
+python bnd.py
+exit
+;;
+esac
+done
+
